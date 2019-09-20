d07188fdf808317d8fbad284b64381a70cda4c16
ChanPoyu <dipsy3333@gmail.com>
2019-09-20 09:11:00 +0900

    template patter added

--- Template/template.dart
+++ Template/template.dart
@@ -1,35 +1,71 @@
+import 'dart:io';
+
 void main () {
+	AbstractDisplay d1 = new CharDisplay("H");
+	AbstractDisplay d2 = new StringDisplay("Hello, Dipsy");
+	AbstractDisplay d3 = new StringDisplay("YOYO");
+
+	d1.display();
+	d2.display();
+	d3.display();
 
 }
 
 abstract class AbstractDisplay {
-	void open() {
-
-	}
-
-	void printt() {
-
-	}
-
-	void close() {
-
+	void open() {}
+	void printt() {}
+	void close() {}
+	void display() {
+		open();
+		for(int i = 0; i < 5; i ++){
+			printt();
+		}
+		close();
 	}
 }
 
-class CharDisplay implements AbstractDisplay{
+class CharDisplay extends AbstractDisplay{
+	String char;
+	CharDisplay(this.char);
 	void open(){
+		stdout.write("<<");
+	}
+	void printt(){
+		stdout.write(char);
+	}
+	void close(){
+		stdout.write(">>\n");
+	}
+}
 
+class StringDisplay extends AbstractDisplay{
+	String string;
+	int width;
+
+	StringDisplay(String string){
+		this.string = string;
+		this.width = this.string.length;
+	}
+
+	void open(){
+		printLine();
 	}
 
 	void printt(){
-
+		print("|${string}|");
 	}
 
-	void close(){
-		
+	void close() {
+		printLine();
 	}
-}
 
-class StringDisplay {
-
+	void printLine(){
+		String output = "";
+		output += "+";
+		for(int i = 0; i < width; i ++){
+			output += "-";
+		}
+		output += "+";
+		print(output);
+	}
 }
